7eed94bf35b08590c1ce2ef1ff6bd74f65368d82
sonal-uchiru <sonal123326@gmail.com>
2021-08-10 22:55:16 +0530

    initial

---
+++ app/src/main/java/com/example/mad_project_design_phase/Side_bar_viewed.java
@@ -0,0 +1,14 @@
+package com.example.mad_project_design_phase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Side_bar_viewed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_side_bar_viewed);
+    }
+}
