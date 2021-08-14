62b34cc86510f9ed99f7b13b4903fc6f3bc991e6
sonal-uchiru <sonal123326@gmail.com>
2021-08-14 11:10:42 +0530

    finallll

---
+++ app/src/main/java/com/example/mad_project_design_phase/Select_card.java
@@ -0,0 +1,14 @@
+package com.example.mad_project_design_phase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Select_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_card);
+    }
+}
---
+++ app/src/main/java/com/example/mad_project_design_phase/card_details.java
@@ -0,0 +1,14 @@
+package com.example.mad_project_design_phase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class card_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_details);
+    }
+}
