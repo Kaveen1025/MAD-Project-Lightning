78aabe96528e4bbe95b61e9ce752d20b7f88f8cc
kavee <IT20185770@my.sliit.lk>
2021-09-19 09:51:12 +0530

    ka

---
+++ app/src/main/java/com/example/mad_project_design_phase/sign_up_page.java
@@ -0,0 +1,14 @@
+package com.example.mad_project_design_phase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_up_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
+}
