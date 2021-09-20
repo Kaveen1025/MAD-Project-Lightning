cccdeb48f5c42128fad769a22ac6395a19a88c51
kavee <IT20185770@my.sliit.lk>
2021-09-20 12:35:29 +0530

    sign up, user profile, edit_profile

--- app/src/main/java/com/example/mad_project_design_phase/edit_profile.java
+++ app/src/main/java/com/example/mad_project_design_phase/edit_profile.java
@@ -1,14 +1,74 @@
 package com.example.mad_project_design_phase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class edit_profile extends AppCompatActivity {
+
+    EditText edit_fn, edit_ln, edit_email,  edit_add,  edit_phone, current_password, new_password,confirm_password;
+    Button update_btn, delete_btn;
+    DatabaseReference dbRef;
+    //    FirebaseDatabase database;
+    FirebaseAuth auth;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_edit_profile);
+        edit_fn = findViewById(R.id.edit_fn);
+        edit_ln = findViewById(R.id.edit_ln);
+        edit_email= findViewById(R.id.edit_email);
+        edit_add = findViewById(R.id.edit_add);
+        edit_phone = findViewById(R.id.edit_phone);
+        current_password = findViewById(R.id.current_password);
+        new_password = findViewById(R.id.new_password);
+        confirm_password = findViewById(R.id.confirm_password);
+        update_btn = findViewById(R.id.update_btn);
+        delete_btn = findViewById(R.id.delete_btn);
+
+
+    }
+
+    public void EditUserProfile(View view){
+
+        dbRef = FirebaseDatabase.getInstance().getReference().child("Customer").child("Customer.id");// id **
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChildren()){
+
+                    edit_fn.setText(dataSnapshot.child(" FirstName").getValue().toString());
+                    edit_ln.setText(dataSnapshot.child("LastName").getValue().toString());
+                    edit_email.setText(dataSnapshot.child(" Email").getValue().toString());
+                    edit_phone.setText(dataSnapshot.child("Address").getValue().toString());
+                    edit_add.setText(dataSnapshot.child("PhoneNumber").getValue().toString());
+
+                }else{
+
+                    Toast.makeText(edit_profile.this, "No Source to display", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mad_project_design_phase/user_profile.java
+++ app/src/main/java/com/example/mad_project_design_phase/user_profile.java
@@ -1,14 +1,71 @@
 package com.example.mad_project_design_phase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class user_profile extends AppCompatActivity {
+
+    EditText et_firstname, et_last_name, et_email, up_phone, up_address;
+    Button up_edit, btn_View_Address;
+    DatabaseReference dbRef;
+    //    FirebaseDatabase database;
+    FirebaseAuth auth;
+    FirebaseUser firebaseUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        et_firstname = findViewById(R.id.et_firstname);
+        et_last_name = findViewById(R.id.et_last_name);
+        et_email= findViewById(R.id.et_email);
+        up_phone = findViewById(R.id.up_phone);
+        up_address = findViewById(R.id.up_address);
+        up_edit = findViewById(R.id.up_edit);
+        btn_View_Address = findViewById(R.id.btn_View_Address);
     }
+
+    public void ViewUserProfile(View view){
+
+        dbRef = FirebaseDatabase.getInstance().getReference().child("Customer").child("Customer.id");// id **
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChildren()){
+
+                   et_firstname.setText(dataSnapshot.child(" FirstName").getValue().toString());
+                   et_last_name.setText(dataSnapshot.child("LastName").getValue().toString());
+                   et_email.setText(dataSnapshot.child(" Email").getValue().toString());
+                   up_phone.setText(dataSnapshot.child("PhoneNumber").getValue().toString());
+                   up_address.setText(dataSnapshot.child("Address").getValue().toString());
+
+                }else{
+
+                    Toast.makeText(user_profile.this, "No Source to display", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
