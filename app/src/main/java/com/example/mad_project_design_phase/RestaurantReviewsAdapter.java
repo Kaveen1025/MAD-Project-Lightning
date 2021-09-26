fc47d234ca2e47525a95dfccebb0dcbaebef4708
sonal-uchiru <sonal123326@gmail.com>
2021-09-26 16:32:38 +0530

    complete

---
+++ app/src/main/java/com/example/mad_project_design_phase/RestaurantReviewsAdapter.java
@@ -0,0 +1,70 @@
+package com.example.mad_project_design_phase;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.jetbrains.annotations.NotNull;
+
+public class RestaurantReviewsAdapter extends FirebaseRecyclerAdapter<CustomerRestaurantReviews, RestaurantReviewsAdapter.myViewHolder> {
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public RestaurantReviewsAdapter(@NonNull @NotNull FirebaseRecyclerOptions<CustomerRestaurantReviews> options) {
+        super(options);
+    }
+
+
+    @Override
+    protected void onBindViewHolder(@NonNull @NotNull RestaurantReviewsAdapter.myViewHolder holder, final int position,
+                                    @NonNull @NotNull CustomerRestaurantReviews model) {
+        holder.name.setText(model.getName());
+        holder.rating.setText(model.getNoOfStars());
+        holder.review.setText(model.getReview());
+
+        Glide.with(holder.img.getContext()).load(model.getCustomerProfile()).placeholder(R.drawable.common_google_signin_btn_icon_dark)
+                .error(R.drawable.common_google_signin_btn_icon_dark_normal).into(holder.img);
+    }
+    @NonNull
+    @NotNull
+    @Override
+    public RestaurantReviewsAdapter.myViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_favorite_rest_card, parent, false);
+        return new RestaurantReviewsAdapter.myViewHolder(view);
+    }
+
+    class myViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView img;
+        TextView name,review,rating;
+
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img = (ImageView)itemView.findViewById(R.id.customerProfile);
+            name = (TextView)itemView.findViewById(R.id.custName);
+            review = (TextView)itemView.findViewById(R.id.review);
+            rating = (TextView)itemView.findViewById(R.id.custRating);
+
+        }
+    }
+
+}
+
+
--- app/src/main/java/com/example/mad_project_design_phase/show_calories.java
+++ app/src/main/java/com/example/mad_project_design_phase/show_calories.java
@@ -2,13 +2,73 @@ package com.example.mad_project_design_phase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.text.BreakIterator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class show_calories extends AppCompatActivity {
 
-    @Override
+    TextView FoodName,TotalCalories;
+    int count = 0;
+    int totalCalories = 0;
+
+    ImageButton closeBtn;
+    int[] optionNames = {
+      R.id.option1Name,R.id.option2Name,R.id.option3Name,R.id.option4Name
+    };
+
+    int[] optionCal = {
+      R.id.option1Cal,R.id.option2Cal,R.id.option3Cal,R.id.option4cal
+    };
+
+
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_calories);
+
+        FoodName = findViewById(R.id.foodName);
+        TotalCalories = findViewById(R.id.TotalCalories);
+        closeBtn = findViewById(R.id.closeBtn22);
+
+        HashMap<String,Integer> j = new HashMap<>();
+       Intent intent = getIntent();
+
+      j.put(intent.getStringExtra("Opt1Value"),Integer.parseInt(intent.getStringExtra("calories1")));
+      j.put(intent.getStringExtra("Opt2Value"),Integer.parseInt(intent.getStringExtra("calories2")));
+      j.put(intent.getStringExtra("Opt3Value"),Integer.parseInt(intent.getStringExtra("calories3")));
+      j.put(intent.getStringExtra("Opt4Value"),Integer.parseInt(intent.getStringExtra("calories4")));
+        for(Map.Entry<String, Integer> set : j.entrySet()) {
+            TextView s = findViewById(optionNames[count]);
+            s.setText(set.getKey());
+            TextView d = findViewById(optionCal[count]);
+            d.setText(String.valueOf(set.getValue()) + " kcal");
+            totalCalories += set.getValue();
+            count++;
+        }
+        TotalCalories.setText(String.valueOf(totalCalories) + " kcal");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(show_calories.this,customise_food.class);
+                startActivity(intent1);
+            }
+        });
+
     }
 }
