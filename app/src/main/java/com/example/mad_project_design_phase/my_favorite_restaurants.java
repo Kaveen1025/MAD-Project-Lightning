e31e74ea0e1f0c69f41099fadfb93b2919424e36
sonal-uchiru <sonal123326@gmail.com>
2021-09-26 16:33:22 +0530

    Heshini Merge

--- app/src/main/java/com/example/mad_project_design_phase/my_favorite_restaurants.java
+++ app/src/main/java/com/example/mad_project_design_phase/my_favorite_restaurants.java
@@ -1,14 +1,50 @@
 package com.example.mad_project_design_phase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class my_favorite_restaurants extends AppCompatActivity {
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class my_favorite_restaurants extends AppCompatActivity {
+    String CustomerID;
+    RecyclerView rv;
+    FRMainAdapter mainAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_favorite_restaurants);
+
+        rv = (RecyclerView) findViewById(R.id.FavRestaurants);
+
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+        FirebaseRecyclerOptions<FavoriteRestaurant> options =
+                new FirebaseRecyclerOptions.Builder<FavoriteRestaurant>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("FavoriteRestaurant").child(CustomerID), FavoriteRestaurant.class)
+                        .build();
+
+
+        mainAdapter = new FRMainAdapter(options);
+
+        rv.setAdapter(mainAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mainAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mainAdapter.stopListening();
     }
 }
