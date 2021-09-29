7e3bb3ece1afbc5cf26e3a426991eb664e4da00d
sonal-uchiru <sonal123326@gmail.com>
2021-09-29 13:01:59 +0530

    Version 2.0.0

--- app/src/main/java/com/example/mad_project_design_phase/favorits_food.java
+++ app/src/main/java/com/example/mad_project_design_phase/favorits_food.java
@@ -12,40 +12,40 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class favorits_food extends AppCompatActivity {
 
-//    RecyclerView recyclerView;
-//    favouriteFood_Adapter adapter;
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_favorits_food);
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference ref = database.getReference();
-//
-//        recyclerView = (RecyclerView) findViewById((R.id.favourite_rv));
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//
-//        FirebaseRecyclerOptions <FavouriteModel> options =
-//                new FirebaseRecyclerOptions.Builder<FavouriteModel>()
-//                .setQuery(FirebaseDatabase.getInstance().getReference().child("FavoriteFood").child("C1"),FavouriteModel.class)
-//                .build();
-//
-//        adapter = new favouriteFood_Adapter(options);
-//        recyclerView.setAdapter(adapter);
-//
-//
-//    }
-//
-//    protected void onStart(){
-//        super.onStart();
-//        adapter.startListening();
-//    }
-//
-//    protected void onStop(){
-//        super.onStop();
-//        adapter.stopListening();
-//    }
+    RecyclerView recyclerView;
+    favouriteFood_Adapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorits_food);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference();
+
+        recyclerView = (RecyclerView) findViewById((R.id.favourite_rv));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions <FavouriteModel> options =
+                new FirebaseRecyclerOptions.Builder<FavouriteModel>()
+                .setQuery(FirebaseDatabase.getInstance().getReference().child("FavoriteFood").child("C1"),FavouriteModel.class)
+                .build();
+
+        adapter = new favouriteFood_Adapter(options);
+        recyclerView.setAdapter(adapter);
+
+
+    }
+
+    protected void onStart(){
+        super.onStart();
+        adapter.startListening();
+    }
+
+    protected void onStop(){
+        super.onStop();
+        adapter.stopListening();
+    }
 
 
 }
