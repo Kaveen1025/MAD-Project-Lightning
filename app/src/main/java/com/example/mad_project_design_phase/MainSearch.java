09176e85cc54b656ad6cfe4b90e31a27b5781dcb
OnaraPathiraja014 <onarap2000@gmail.com>
2021-10-01 00:02:19 +0530

    Search

--- app/src/main/java/com/example/mad_project_design_phase/MainSearch.java
+++ app/src/main/java/com/example/mad_project_design_phase/MainSearch.java
@@ -1,14 +1,92 @@
 package com.example.mad_project_design_phase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
 
-public class MainSearch extends AppCompatActivity {
+public class MainSearch extends AppCompatActivity implements View.OnClickListener{
+
+    public CardView itl, jap, ind, amr, vege, other;
+    SearchView search;
+    Button submit;
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_search);
+        i = new Intent(MainSearch.this,all_the_restaurants.class);
+        search = (SearchView) findViewById(R.id.searchView);
+        submit = (Button) findViewById(R.id.searchSubmit);
+
+        itl = (CardView) findViewById(R.id.italian);
+        jap = (CardView) findViewById(R.id.japanese);
+        ind = (CardView) findViewById(R.id.indian);
+        amr = (CardView) findViewById(R.id.american);
+        vege = (CardView) findViewById(R.id.vegetarian);
+        other = (CardView) findViewById(R.id.others);
+
+        itl.setOnClickListener(this);
+        jap.setOnClickListener(this);
+        ind.setOnClickListener(this);
+        amr.setOnClickListener(this);
+        vege.setOnClickListener(this);
+        other.setOnClickListener(this);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String type = search.getQuery().toString();
+                i.putExtra("search",type);
+                startActivity(i);
+
+            }
+        });
+
     }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.italian:
+                i.putExtra("filter", "Italian");
+                startActivity(i);
+                break;
+
+            case R.id.japanese:
+                i.putExtra("filter", "Japanese");
+                startActivity(i);
+                break;
+
+            case R.id.american:
+                i.putExtra("filter", "American");
+                startActivity(i);
+                break;
+
+            case R.id.indian:
+                i.putExtra("filter", "Indian");
+                startActivity(i);
+                break;
+
+            case R.id.vegetarian:
+                i.putExtra("filter", "Vegetarian");
+                startActivity(i);
+                break;
+
+            case R.id.others:
+                i.putExtra("filter", "Other");
+                startActivity(i);
+                break;
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/mad_project_design_phase/all_the_restaurants.java
+++ app/src/main/java/com/example/mad_project_design_phase/all_the_restaurants.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
@@ -15,26 +17,62 @@ public class all_the_restaurants extends AppCompatActivity {
 
     RestMainAdapter mainAdapter;
 
+    Intent i;
+    String startName,orderBy,endName ;
+    int flag =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_the_restaurants);
+        i = getIntent();
+//        if(i.getStringExtra("filter").toString().equals("")){
+//            Log.i("msg","msg");
+//        }else if(!i.getStringExtra("filter").toString().equals("")){
+//            Log.i("msg","msg1");
+//        }
+
+        if(i.hasExtra("filter")){
+
+            startName = i.getStringExtra("filter");
+            orderBy = "cuisineType";
+            endName = startName + "~";
+            Log.i("msg",endName);
+
+        }else if(i.hasExtra("search")){
+            startName = i.getStringExtra("search");
+            orderBy = "name";
+            endName = startName + "~";
+            Log.i("msg","msg1");
+        }else{
+           flag =1;
+        }
 
         //Assigning recycler view
         rv = (RecyclerView) findViewById(R.id.restRecycler);
 
         rv.setLayoutManager(new LinearLayoutManager(this));
+        FirebaseRecyclerOptions<AllRestaurant> options ;
+        if(flag == 0) {
+            options =
+                    new FirebaseRecyclerOptions.Builder<AllRestaurant>()
+                            .setQuery(FirebaseDatabase.getInstance().getReference().child("Restaurant").orderByChild(orderBy).startAt(startName).endAt(endName), AllRestaurant.class)
+                            .build();
+            mainAdapter = new RestMainAdapter(options);
 
+            rv.setAdapter(mainAdapter);
+        }else if(flag ==1){
+            options =
+                    new FirebaseRecyclerOptions.Builder<AllRestaurant>()
+                            .setQuery(FirebaseDatabase.getInstance().getReference().child("Restaurant"), AllRestaurant.class)
+                            .build();
+            mainAdapter = new RestMainAdapter(options);
 
-        FirebaseRecyclerOptions<AllRestaurant> options =
-                new FirebaseRecyclerOptions.Builder<AllRestaurant>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Restaurant").orderByChild("name").startAt("Western").endAt("Western~"), AllRestaurant.class)
-                        .build();
+            rv.setAdapter(mainAdapter);
+        }
 
-
-        mainAdapter = new RestMainAdapter(options);
-
-        rv.setAdapter(mainAdapter);
+//        mainAdapter = new RestMainAdapter(options);
+//
+//        rv.setAdapter(mainAdapter);
     }
 
     @Override
